9a6cbafc0b4791056747170dc794ac242e3bff73
amanr11314 <amanr11314@gmail.com>
2021-10-25 12:26:19 +0530

    landing page - progress 80% - css setup done with webpack - landing page almost done only footer remaining

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -16,6 +16,26 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      // CSS rules
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
+      },
     ],
   },
   optimization: {
